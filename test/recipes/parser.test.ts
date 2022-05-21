3c9e4578c9dd004a4bf8a0016d681946eb3566da
Benjamin Caldwell <benjamin@bcaldwell.ca>
2022-05-21 22:16:03 +0000

    Fix microdata case

--- test/recipes/parser.test.ts
+++ test/recipes/parser.test.ts
@@ -171,4 +171,46 @@ describe("Recipe Parser", () => {
       expect(recipe).to.deep.equal(expectedRecipe)
     })
   })
+  describe("Parse microdata", function () {
+    it("returns correct recipe", async function () {
+      const recipe = await parseRecipeFromURL("https://www.ethanchlebowski.com/cooking-techniques-recipes/parathas-the-flaky-flatbread")
+      const expectedRecipe: Recipe = {
+        name: "Parathas",
+        author: "Ethan Chlebowski",
+        image: "https://i.imgur.com/cgF2QPE.jpg",
+        instructions: [
+          {
+            title: "",
+            isMain: true,
+            instructions: [
+              "Set a bowl over a scale and the flour, salt, and oil. Coarsely mix with your hands until evenly distributed and the oil coats the flour. Add the water and mix with your hands until no dry flour remains in the bowl. Set a lid or plastic wrap over the top and let rest for 15 minutes.",
+              "Once rested, the dough should be well relaxed. Knead for about 5 minutes until the dough passes the gluten window test. Using a scale, divide the dough into 8 pieces for larger paratha or 10 pieces for smaller paratha. Spread out evenly on a plate and cover. Let rest for 15 more minutes.",
+              "Sprinkle some flour on the work surface and your rolling pin. For each piece, roll the dough into a circle as thin as you can (it should be slightly translucent). Spread some of the softened ghee all over the dough then sprinkle a little more flour over the ghee.",
+              "Fold the dough in roughly 1-inch increments over onto itself creating multiple layers. After folding you should have one long rectangle with several layers of dough & ghee. Roll the layered rectangle into a tight circle and seal the end of it together. Press the circle down with your hands and repeat this process with the remaining pieces of dough. Let rest for 15 minutes.",
+              "Use flour as needed to prevent sticking, roll each circle of coiled dough into a circle about 8 inches in diameter. Do the first cook right away and repeat with each piece.",
+              "Place some ghee in a pan over medium heat. Once melted add the paratha and cook on one side until it is golden brown, a couple of dark spots are fine. Flip the paratha and cook until the other side is golden brown. Parathas can be stored in aluminum foil for a short time before serving with your meal.",
+              "For the first cook, set a pan over medium-low heat. Place the paratha down and let cook for about 2-3 minutes per side until no dry spots remain. Once cooked transfer to a wire rack to cool and cook the remaining parathas.",
+              "Once all the paratha are cooked and cooled, place them in a container or freezer bag separated by parchment paper to avoid sticking. Place in the freezer until ready to use.",
+              "To cook from frozen, set a pan over medium heat and add some ghee or oil. Once hot, add the paratha and cook until crispy and golden on one side, a couple of dark spots are good! Flip and cook until the other side is frozen. Eat right away.",
+            ],
+          },
+        ],
+        keywords: [],
+        category: "",
+        ingredients: [
+          "500 g all-purpose flour (or a 50% mix with whole wheat)",
+          "10 g of salt",
+          "30 g vegetable oil",
+          "300 g water",
+          "Flour for rolling out, as needed",
+          "60 g softened ghee (or butter) - for spreading on rolled-out dough.",
+        ],
+        cuisine: "",
+        time: "Total time: 1 Hour",
+        url: "https://www.ethanchlebowski.com/cooking-techniques-recipes/parathas-the-flaky-flatbread",
+        description: "",
+      }
+      expect(recipe).to.deep.equal(expectedRecipe)
+    })
+  })
 })
